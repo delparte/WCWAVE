a7dd75c33c9e25905593976885dbf7e08acae17a
Tucker Chapman <tucker.r.chapman@gmail.com>
2015-09-11 21:08:49 -0600

    Add OLS to precip mass for JD

--- gridtools.py
+++ gridtools.py
@@ -230,31 +230,52 @@ def precipMass():
             cursor.deleteRow(row)
     del cursor
     del row
-
     #Add unique ID field to tempStations for use in OLS function
     arcpy.AddField_management(scratchGDB +"/tempStations", "Unique_ID", "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
     arcpy.CalculateField_management(scratchGDB +"/tempStations","Unique_ID","!OBJECTID!","PYTHON_9.3","")
-    #Run ordinary least squares on tempStations
-    arcpy.CreateTable_management(scratchGDB, "coef_table")
-    ols = arcpy.OrdinaryLeastSquares_stats(scratchGDB + "/tempStations","Unique_ID","in_memory/fcResid","MEAN_ppts","RASTERVALU", scratchGDB + "/coef_table","","")
-    lsScratchData_Imd.append(scratchGDB + "/coef_table")
-    intercept = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[0]
-    slope = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[1]
-    #Calculate residuals and add them to tempStations
-    arcpy.AddField_management(scratchGDB + "/tempStations", "residual", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-    cursor = arcpy.UpdateCursor(scratchGDB + "/tempStations")
-    for row in cursor:
-        row.setValue("residual", row.getValue("MEAN_ppts") - ((slope * row.getValue("RASTERVALU")) + intercept))
-        cursor.updateRow(row)
-    del cursor
-    del row
-    arcpy.EmpiricalBayesianKriging_ga(in_features=scratchGDB + "/tempStations", z_field="residual", out_ga_layer="#", \
-        out_raster=scratchGDB + "/precipitation_residual", cell_size=output_cell_size, transformation_type="NONE", max_local_points="100", \
-        overlap_factor="1", number_semivariograms="100", search_neighborhood="NBRTYPE=SmoothCircular RADIUS=10000.9518700025 SMOOTH_FACTOR=0.2", \
-        output_type="PREDICTION", quantile_value="0.5", threshold_type="EXCEED", probability_threshold="", semivariogram_model_type="THIN_PLATE_SPLINE")
-    #Add back elevation trends and save final raster
-    output_raster = arcpy.Raster(scratchGDB + "/precipitation_residual") + (arcpy.Raster(rc_elevation) * slope + intercept)
-    output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
+
+    if sWatershed == "Johnston Draw":
+        cursor = arcpy.SearchCursor(scratchGDB + "/tempStations")
+        x = []
+        y = []
+        for row in cursor:
+            x.append(row.getValue('RASTERVALU'))
+            y.append(row.getValue('MEAN_ppts'))
+        del cursor
+        del row
+        A = numpy.vstack([x,numpy.ones(len(x))]).T
+        slope, intercept = numpy.linalg.lstsq(A, y)[0]
+        arcpy.AddMessage('Slope ' + str(slope) + ', Intercept ' + str(intercept))
+        if slope != 0.0 and intercept != 0.0:
+            #Create final raster
+            arcpy.env.extent = extElevation
+            output_raster = (Raster(rc_elevation) * slope + intercept)
+            output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
+        else:
+            return
+    else:
+        #Run ordinary least squares on tempStations
+        arcpy.CreateTable_management(scratchGDB, "coef_table")
+        ols = arcpy.OrdinaryLeastSquares_stats(scratchGDB + "/tempStations","Unique_ID","in_memory/fcResid","MEAN_ppts","RASTERVALU", scratchGDB + "/coef_table","","")
+        lsScratchData_Imd.append(scratchGDB + "/coef_table")
+        intercept = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[0]
+        slope = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[1]
+
+        #Calculate residuals and add them to tempStations
+        arcpy.AddField_management(scratchGDB + "/tempStations", "residual", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+        cursor = arcpy.UpdateCursor(scratchGDB + "/tempStations")
+        for row in cursor:
+            row.setValue("residual", row.getValue("MEAN_ppts") - ((slope * row.getValue("RASTERVALU")) + intercept))
+            cursor.updateRow(row)
+        del cursor
+        del row
+        arcpy.EmpiricalBayesianKriging_ga(in_features=scratchGDB + "/tempStations", z_field="residual", out_ga_layer="#", \
+            out_raster=scratchGDB + "/precipitation_residual", cell_size=output_cell_size, transformation_type="NONE", max_local_points="100", \
+            overlap_factor="1", number_semivariograms="100", search_neighborhood="NBRTYPE=SmoothCircular RADIUS=10000.9518700025 SMOOTH_FACTOR=0.2", \
+            output_type="PREDICTION", quantile_value="0.5", threshold_type="EXCEED", probability_threshold="", semivariogram_model_type="THIN_PLATE_SPLINE")
+        #Add back elevation trends and save final raster
+        output_raster = arcpy.Raster(scratchGDB + "/precipitation_residual") + (arcpy.Raster(rc_elevation) * slope + intercept)
+        output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
 
     return outFolder + "/precipitation_mass_" + sTimeStamp + ".tif"
 
