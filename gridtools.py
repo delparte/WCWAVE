bb5c3119844c943910801343ae1c10ab1af283aa
Tucker Chapman <tucker.r.chapman@gmail.com>
2015-09-18 15:23:44 -0600

    Combined, EBK, Detrened choices to precipitation mass

--- gridtools.py
+++ gridtools.py
@@ -333,37 +333,77 @@ def precipMass():
         else:
             return
     else:
-        #Run ordinary least squares on tempStations
-        arcpy.CreateTable_management(scratchGDB, "coef_table")
-        #Check if there was precipitation - I would like to change this check to occur before trying to run OLS
-        try:
+        #Check if interpolation method is Empirical Bayesian or Detrended
+        if sKrigMethod == "Detrended":
+            #Add unique ID field to tempStations for use in OLS function
+            arcpy.AddField_management(scratchGDB +"/tempStations", "Unique_ID", "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+            arcpy.CalculateField_management(scratchGDB +"/tempStations","Unique_ID","!OBJECTID!","PYTHON_9.3","")
+            #Run ordinary least squares on tempStations
+            arcpy.CreateTable_management(scratchGDB, "coef_table")
             ols = arcpy.OrdinaryLeastSquares_stats(scratchGDB + "/tempStations","Unique_ID","in_memory/fcResid","MEAN_ppts","RASTERVALU", scratchGDB + "/coef_table","","")
-        except arcpy.ExecuteError:
-            msgs = arcpy.GetMessages(2)
-            #arcpy.AddMessage(msgs)
-            if 'Zero variance' in msgs:
-                arcpy.AddMessage("No precipitation")
-            return
-        lsScratchData_Imd.append(scratchGDB + "/coef_table")
-        intercept = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[0]
-        slope = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[1]
+            lsScratchData_Imd.append(scratchGDB + "/coef_table")
+            intercept = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[0]
+            slope = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[1]
+            #Calculate residuals and add them to tempStations
+            arcpy.AddField_management(scratchGDB + "/tempStations", "residual", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+            cursor = arcpy.UpdateCursor(scratchGDB + "/tempStations")
+            for row in cursor:
+                row.setValue("residual", row.getValue("MEAN_ppts") - ((slope * row.getValue("RASTERVALU")) + intercept))
+                cursor.updateRow(row)
+            del cursor
+            del row
+            #Run ordinary kriging on residuals
+            #arcpy.gp.Kriging_sa(scratchGDB + "/tempStations", "residual", scratchGDB + "/precipitation_residual", "Linear 37.061494", output_cell_size, "VARIABLE 100", "")
+            outKrig = Kriging(scratchGDB + "/tempStations", "residual", KrigingModelOrdinary("SPHERICAL", 460, 3686, .1214, .2192), output_cell_size, RadiusFixed(10000, 1))
+            outKrig.save(scratchGDB + "/precipitation_residual")
+            lsScratchData_Imd.append(scratchGDB + "/precipitation_residual")
+            #Add back elevation trends and save final raster
+            output_raster = arcpy.Raster(scratchGDB + "/precipitation_residual") + (arcpy.Raster(rc_elevation) * slope + intercept)
+            output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
+        elif sKrigMethod == "Combined":
+            #Run ordinary least squares on tempStations
+            arcpy.CreateTable_management(scratchGDB, "coef_table")
+            #Check if there was precipitation - I would like to change this check to occur before trying to run OLS
+            try:
+                ols = arcpy.OrdinaryLeastSquares_stats(scratchGDB + "/tempStations","Unique_ID","in_memory/fcResid","MEAN_ppts","RASTERVALU", scratchGDB + "/coef_table","","")
+            except arcpy.ExecuteError:
+                msgs = arcpy.GetMessages(2)
+                #arcpy.AddMessage(msgs)
+                if 'Zero variance' in msgs:
+                    arcpy.AddMessage("No precipitation")
+                else: 
+                    arcpy.AddMessage(msgs)
+                return
+            lsScratchData_Imd.append(scratchGDB + "/coef_table")
+            intercept = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[0]
+            slope = list((row.getValue("Coef") for row in arcpy.SearchCursor(scratchGDB + "/coef_table",fields="Coef")))[1]
 
-        #Calculate residuals and add them to tempStations
-        arcpy.AddField_management(scratchGDB + "/tempStations", "residual", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-        cursor = arcpy.UpdateCursor(scratchGDB + "/tempStations")
-        for row in cursor:
-            row.setValue("residual", row.getValue("MEAN_ppts") - ((slope * row.getValue("RASTERVALU")) + intercept))
-            cursor.updateRow(row)
-        del cursor
-        del row
-        arcpy.EmpiricalBayesianKriging_ga(in_features=scratchGDB + "/tempStations", z_field="residual", out_ga_layer="#", \
-            out_raster=scratchGDB + "/precipitation_residual", cell_size=output_cell_size, transformation_type="NONE", max_local_points="100", \
-            overlap_factor="1", number_semivariograms="100", search_neighborhood="NBRTYPE=SmoothCircular RADIUS=10000.9518700025 SMOOTH_FACTOR=0.2", \
-            output_type="PREDICTION", quantile_value="0.5", threshold_type="EXCEED", probability_threshold="", semivariogram_model_type="THIN_PLATE_SPLINE")
-        #Add back elevation trends and save final raster
-        output_raster = arcpy.Raster(scratchGDB + "/precipitation_residual") + (arcpy.Raster(rc_elevation) * slope + intercept)
-        output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
+            #Calculate residuals and add them to tempStations
+            arcpy.AddField_management(scratchGDB + "/tempStations", "residual", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+            cursor = arcpy.UpdateCursor(scratchGDB + "/tempStations")
+            for row in cursor:
+                row.setValue("residual", row.getValue("MEAN_ppts") - ((slope * row.getValue("RASTERVALU")) + intercept))
+                cursor.updateRow(row)
+            del cursor
+            del row
+            arcpy.EmpiricalBayesianKriging_ga(in_features=scratchGDB + "/tempStations", z_field="residual", out_ga_layer="#", \
+                out_raster=scratchGDB + "/precipitation_residual", cell_size=output_cell_size, transformation_type="NONE",\
+                max_local_points="100", overlap_factor="1", number_semivariograms="100", \
+                search_neighborhood="NBRTYPE=SmoothCircular RADIUS=10000.9518700025 SMOOTH_FACTOR=0.2", \
+                output_type="PREDICTION", quantile_value="0.5", threshold_type="EXCEED", probability_threshold="", \
+                semivariogram_model_type="THIN_PLATE_SPLINE")
+            #Add back elevation trends and save final raster
+            output_raster = arcpy.Raster(scratchGDB + "/precipitation_residual") + (arcpy.Raster(rc_elevation) * slope + intercept)
+            output_raster.save(outFolder + "/precipitation_mass_" + sTimeStamp + ".tif")
+        else:
+            arcpy.EmpiricalBayesianKriging_ga(in_features=scratchGDB + "/tempStations", z_field="MEAN_ppts", out_ga_layer="#", \
+                out_raster=outFolder + "/precipitation_mass_scratch_" + sTimeStamp + ".tif", cell_size=output_cell_size, \
+                transformation_type="EMPIRICAL", max_local_points="100", overlap_factor="1", number_semivariograms="100", \
+                search_neighborhood="NBRTYPE=SmoothCircular RADIUS=10000.9518700025 SMOOTH_FACTOR=0.2", \
+                output_type="PREDICTION", quantile_value="0.5", threshold_type="EXCEED", probability_threshold="", \
+                semivariogram_model_type="WHITTLE_DETRENDED")
 
+    
     return outFolder + "/precipitation_mass_" + sTimeStamp + ".tif"
 
 def snowDepth():
@@ -886,7 +926,7 @@ if sWatershed == "Johnston Draw":
     db = 'jd_data'
 
 elif sWatershed == "Reynolds Creek":
-    arcpy.AddMessage("Reynolds Creed Watershed")
+    arcpy.AddMessage("Reynolds Creek Watershed")
     stations = r'C:\ReynoldsCreek\Relevant_Data.gdb\station_locations'
     elev_tiff = r'C:\ReynoldsCreek\rc_elevation_filled.tif'
     dem = r'C:\ReynoldsCreek\Relevant_Data.gdb\RC_DEM_10m_South'
